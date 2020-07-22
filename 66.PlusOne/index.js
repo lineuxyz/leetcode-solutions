2289251240a19071d9f578ca3445725425e25bfd
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-22 10:23:32 -0300

    add the problem solution

---
+++ 66.PlusOne/index.js
@@ -0,0 +1,16 @@
+var plusOne = function(digits) {
+  let carry = 0;
+for (let i = digits.length - 1 ; i >= 0; i--){
+   carry = (digits[i] + 1 == 10)? 1 : 0;
+   if (carry == 0){
+       digits[i] = digits[i] + 1;
+       break;
+   }else{
+       digits[i] = 0 ; 
+   }
+}
+if (carry == 1){
+   digits.unshift(1);
+}
+return digits;
+};

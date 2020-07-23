adc1538437a7f1e729148ee0639f66c555726dab
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-23 11:29:17 -0300

    add the problem solution

---
+++ 118.PascalTriangle/index.js
@@ -0,0 +1,12 @@
+var generate = function(numRows) {
+  var result = [];
+  for (var i = 0, l = numRows; i < l; i++) {
+      var arr = [];
+      arr[0] = arr[i] = 1;
+      for (var j = 1; j < i; j++) {
+          arr[j] = result[i - 1][j] + result[i - 1][j - 1];
+      }
+      result[i] = arr;
+  }
+  return result;
+};

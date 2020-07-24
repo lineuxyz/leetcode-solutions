28558ff05bc40441c4e3caec550ef24b8f8f3b20
Lineu Pastorelli <lineualberto@gmail.com>
2020-07-24 11:15:50 -0300

    add the javascript solution problem

---
+++ 125.ValidPalindrome/index.js
@@ -0,0 +1,12 @@
+var isPalindrome = function(s) {
+  s = s.replace(/[^a-zA-Z0-9]/g,'')
+  s = s.toLowerCase()
+  for (let [i,j]=[0,s.length-1]; i<j;) {
+      if (s[i]!=s[j]) {
+          return false
+      }
+      i++
+      j--
+  }
+  return true
+}
